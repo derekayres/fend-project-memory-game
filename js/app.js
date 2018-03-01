84554e69ccd66b998a31c9f871ff1fd1eaf45c15
Derek Ayres <wunderlad@hotmail.com>
2018-03-01 13:45:32 -0500

    Stopwatch

--- js/app.js
+++ js/app.js
@@ -20,6 +20,29 @@ const arrayClassNames = [
   "fa fa-bomb"
 ]
 
+var h2 = document.getElementsByTagName('h2')[0],
+start = document.getElementById('start'),
+//stop = document.getElementById('stop'),
+//clear = document.getElementById('clear'),
+seconds = 0, minutes = 0, hours = 0, t;
+
+function add() {
+    seconds++;
+    if (seconds >= 60) {
+        seconds = 0;
+        minutes++;
+        if (minutes >= 60) {
+            minutes = 0;
+            hours++;
+        }
+    }
+    h2.textContent = (hours ? (hours > 9 ? hours : "0" + hours) : "00") + ":" + (minutes ? (minutes > 9 ? minutes : "0" + minutes) : "00") + ":" + (seconds > 9 ? seconds : "0" + seconds);
+    timer();
+}
+function timer() {
+    t = setTimeout(add, 1000);
+}
+start.onclick = timer;
 /*
  * Display the cards on the page
  *   - shuffle the list of cards using the provided "shuffle" method below
@@ -38,7 +61,6 @@ function shuffle(array) {
         array[currentIndex] = array[randomIndex];
         array[randomIndex] = temporaryValue;
     }
-
     return array;
 }
 
@@ -46,6 +68,7 @@ const shuffledArray = shuffle(arrayClassNames);
 const deck = document.querySelector(".deck");
 const newDeck = document.querySelector('.deck');
 let cardOne = null;
+
 
 function makeCards(shuffledArray) {
   deck.innerHTML="";
@@ -63,8 +86,8 @@ function makeCards(shuffledArray) {
 };
 
 function checkCard() {
-    newCard.removeEventListener('click', checkCard);
     const clickedCard = this
+    clickedCard.removeEventListener('click', checkCard);
     clickedCard.classList.add('open');
     clickedCard.classList.add('show');
     //cardOne = this;
@@ -87,7 +110,7 @@ function checkCard() {
       cardOne.classList.remove('show');
       cardOne.addEventListener('click', checkCard);
       cardOne = null;
-      }, 3000);
+    }, 2000);
     }
       console.log ('It worked.')
       console.dir(this)
@@ -96,6 +119,17 @@ function checkCard() {
 makeCards(shuffledArray);
 
 
+/* Stop button
+stop.onclick = function() {
+    clearTimeout(t);
+}
+
+Clear button
+clear.onclick = function() {
+    h1.textContent = "00:00:00";
+    seconds = 0; minutes = 0; hours = 0;
+}
+*/
 
 
 
